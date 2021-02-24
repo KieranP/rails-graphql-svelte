5a0609439a14d3ed4f608793d191b7d83fb8767b
Kieran Pilkington <kieran776@gmail.com>
2021-02-24 20:12:32 +1300

    Fix CSP configs

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,7 +1,7 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :none
   policy.img_src :self
-  policy.style_src :self, (:unsafe_inline if Rails.env.development?)
-  policy.script_src :self, (:unsafe_inline if Rails.env.development?)
-  policy.connect_src :self, ('ws://localhost:12321' if Rails.env.development?)
+  policy.style_src *[:self, (:unsafe_inline if Rails.env.development?)].compact
+  policy.script_src *[:self, (:unsafe_inline if Rails.env.development?)].compact
+  policy.connect_src *[:self, ('ws://localhost:12321' if Rails.env.development?)].compact
 end
