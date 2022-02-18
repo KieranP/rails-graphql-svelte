be2cadab45e53287ce0ff0aae64cd7254521a220
Kieran Pilkington <kieran776@gmail.com>
2022-02-18 20:10:46 +1300

    Renable brakeman when committing

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,12 @@ class User < ApplicationRecord
   include HasUuid
 
   PASSWORD_FORMAT = /\A
-    (?=.{8,})          # Must contain 8 or more characters
     (?=.*\d)           # Must contain a digit
     (?=.*[a-z])        # Must contain a lower case character
     (?=.*[A-Z])        # Must contain an upper case character
     (?=.*[[:^alnum:]]) # Must contain a symbol
-  /x
+    .{8,}              # Must contain 8 or more characters
+  \z/x
 
   has_secure_password
 
